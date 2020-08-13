ad592daadc7ef2ac48a82c287a815d709a85feb6
stsan9 <stsan@ucsd.edu>
2020-08-13 04:25:53 +0000

    ls

---
+++ code/gnn_process_datasets.py
@@ -0,0 +1,4 @@
+from graph_data import GraphDataset
+bb1_data = GraphDataset(root='/anomalyvol/data/gnn_geom/full', bb=1)
+bb2_data = GraphDataset(root='/anomalyvol/data/gnn_geom/full', bb=2)
+bb3_data = GraphDataset(root='/anomalyvol/data/gnn_geom/full', bb=3)
--- code/graph_data.py
+++ code/graph_data.py
@@ -34,15 +34,15 @@ class GraphDataset(Dataset):
 
     @property
     def processed_file_names(self):
-        njets = 24043
-        if self.start != 0 and self.stop!=-1:
-            njets = self.stop - self.start
-        
-        # possible boxes to read
+        # possible file formats of boxes to read
         file_string = ['data_{}.pt', 'data_bb1_{}.pt', 'data_bb2_{}.pt', 'data_bb3_{}.pt']
-        
-        #files = [osp.basename(x) for x in glob(osp.join(self.processed_dir, file_string[self.bb]))]
-        return [file_string[self.bb].format(i) for i in range(njets)]
+
+        if self.start!=0 and self.stop!=-1:
+            njetpairs = self.stop-self.start
+            return [file_string[self.bb].format(i) for i in range(self.start,self.stop)]
+        else:
+            njets = 24043
+            return [file_string[self.bb].format(i) for i in range(njets)]
 
     def __len__(self):
         return len(self.processed_file_names)
@@ -61,19 +61,21 @@ class GraphDataset(Dataset):
             total_size = 1000000
             chunk_size = 100000
 
-        data = []
-        nonzero_particles = []
-        event_indices = []
-        masses = []
-        px = []
-        py = []
-        pz = []
-        e = []
-        event_idx = 0
-
         for raw_path in self.raw_paths:
-            for k in range(total_size // chunksize - 1):
-                df = pd.read_hdf(path, start = k * chunk_size, stop = (k + 1) * chunk_size)
+            event_idx = 0
+            ijet = 0
+            for k in range(total_size // chunk_size - 1):
+                
+                data = []
+                nonzero_particles = []
+                event_indices = []
+                masses = []
+                px = []
+                py = []
+                pz = []
+                e = []
+        
+                df = pd.read_hdf(raw_path, start = k * chunk_size, stop = (k + 1) * chunk_size)
                 all_events = df.values
                 rows = all_events.shape[0]
                 cols = all_events.shape[1]
@@ -116,27 +118,26 @@ class GraphDataset(Dataset):
                 
                     event_idx += 1
 
-        file_string = ['data_{}.pt', 'data_bb1_{}.pt', 'data_bb2_{}.pt', 'data_bb3_{}.pt']
-        ijet = 0
-        for data_idx, d in enumerate(data):
-            n_particles = nonzero_particles[ijet]
-            pairs = [[i, j] for (i, j) in itertools.product(range(n_particles),range(n_particles)) if i!=j]
-            edge_index = torch.tensor(pairs, dtype=torch.long)
-            edge_index=edge_index.t().contiguous()
-            x = torch.tensor(d, dtype=torch.float)
-            y = torch.tensor(d, dtype=torch.float)
-            data = Data(x=x, edge_index=edge_index, y=y)
-            if self.pre_filter is not None and not self.pre_filter(data):
-                continue
-            if self.pre_transform is not None:
-                data = self.pre_transform(data)
-            
-            # save data in format (jet_Data, event_of_jet, mass_of_jet, px, py, pz, e)
-            torch.save((data, event_indices[data_idx],
-                        masses[data_idx], px[data_idx],
-                        py[data_idx], pz[data_idx],
-                        e[data_idx]), osp.join(self.processed_dir, file_string[self.bb].format(ijet)))
-            ijet += 1
+                file_string = ['data_{}.pt', 'data_bb1_{}.pt', 'data_bb2_{}.pt', 'data_bb3_{}.pt']
+                for data_idx, d in enumerate(data):
+                    n_particles = nonzero_particles[ijet]
+                    pairs = [[i, j] for (i, j) in itertools.product(range(n_particles),range(n_particles)) if i!=j]
+                    edge_index = torch.tensor(pairs, dtype=torch.long)
+                    edge_index=edge_index.t().contiguous()
+                    x = torch.tensor(d, dtype=torch.float)
+                    y = torch.tensor(d, dtype=torch.float)
+                    data = Data(x=x, edge_index=edge_index, y=y)
+                    if self.pre_filter is not None and not self.pre_filter(data):
+                        continue
+                    if self.pre_transform is not None:
+                        data = self.pre_transform(data)
+
+                    # save data in format (jet_Data, event_of_jet, mass_of_jet, px, py, pz, e)
+                    torch.save((data, event_indices[data_idx],
+                                masses[data_idx], px[data_idx],
+                                py[data_idx], pz[data_idx],
+                                e[data_idx]), osp.join(self.processed_dir, file_string[self.bb].format(ijet)))
+                    ijet += 1
 
     def get(self, idx):
         file_string = ['data_{}.pt', 'data_bb1_{}.pt', 'data_bb2_{}.pt', 'data_bb3_{}.pt']
