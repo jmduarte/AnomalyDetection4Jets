1f6bb1b220b0b5155f0dd632657b5796074fbb6f
Steven Tsan <42155174+stsan9@users.noreply.github.com>
2020-08-18 15:35:19 -0700

    preprocess all boxes

--- code/gnn_process_datasets.py
+++ code/gnn_process_datasets.py
@@ -1,2 +1,5 @@
 from graph_data import GraphDataset
-bb0_data = GraphDataset(root='/anomalyvol/data/gnn_geom', bb=0)
+bb0_data = GraphDataset(root='/anomalyvol/data/gnn_geom/full', bb=0, n_events=1000000)
+bb1_data = GraphDataset(root='/anomalyvol/data/gnn_geom/full', bb=1, n_events=1000000)
+bb2_data = GraphDataset(root='/anomalyvol/data/gnn_geom/full', bb=2, n_events=1000000)
+bb3_data = GraphDataset(root='/anomalyvol/data/gnn_geom/full', bb=3, n_events=1000000)
